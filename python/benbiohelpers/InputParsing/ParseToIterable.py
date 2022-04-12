79050be0b221d083df3269e2e2b3f427592cc531
Benjamin Morledge-Hampton <brotherbenben@yahoo.com>
2022-04-12 14:21:46 -0700

    Create ParseToIterable.py
    Created a general function for parsing complex strings representing ranges/lists to a single iterable.  For example, "1, 3, 9, 4-6, 10" can be parsed to [1,3,9,4,5,6,10].

---
+++ python/benbiohelpers/InputParsing/ParseToIterable.py
@@ -0,0 +1,28 @@
+import warnings
+
+# Make a function for parsing input in range or list format.
+def parseToIterable(input: str, rangeChar = '-', sepChar = ',', castValuesToInt = True):
+
+    # Remove trailing and leading whitespace
+    input = input.strip()
+
+    # If there are separator characters in the input, split it into multiple inputs,
+    # and recursively perform this function on each, concatenating the results into a single list.
+    if sepChar in input:
+        return([
+            item for inputIterable in input.split(sepChar)
+                for item in parseToIterable(inputIterable, rangeChar, sepChar, castValuesToInt)
+        ])
+
+    # If there is a range separator in the input (and no separator characters),
+    # return a range object as the iterable.
+    elif rangeChar in input:
+        start, stop = input.split(rangeChar)
+        if int(stop) < int(start): warnings.warn(f"Stop comes before start in range {input}.  Range will contain no values.")
+        return(range(int(start),int(stop)+1))
+
+    # Otherwise, the input is a single item that just needs to be turned into an iterable and returned.
+    else:
+        if castValuesToInt:
+            return([int(input)])
+        else: return([input])
