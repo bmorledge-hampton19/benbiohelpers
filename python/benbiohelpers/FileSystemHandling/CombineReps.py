e4f9bc0ee2b3c08516fc239f01680735901f322b
Benjamin Morledge-Hampton <brotherbenben@yahoo.com>
2022-07-05 15:17:26 -0700

    More UI params for CombineReps
    - Added parameter for the repetition string basename (e.g. "rep")
    - Added parameter for the combined repetition name
    - Apparently, I don't know how to spell "repetition"...

--- python/benbiohelpers/FileSystemHandling/CombineReps.py
+++ python/benbiohelpers/FileSystemHandling/CombineReps.py
@@ -1,6 +1,6 @@
-# This script combines repititions within a directory using unique repitition naming strings.
+# This script combines repetitions within a directory using unique repetition naming strings.
 # For example, upon finding a file with "rep1" in the name, it looks for one with "rep2", "rep3", etc.
-# and combines them into one file with a new repitition naming string.
+# and combines them into one file with a new repetition naming string.
 # Also recurses into any other directories it encounters along the way.
 
 import gzip
@@ -8,9 +8,9 @@ import os, shutil, warnings
 from benbiohelpers.TkWrappers.TkinterDialog import TkinterDialog
 
 
-def combineReps(parentDirectory, expectedRepititions, repititionStringBase = "rep", combinedRepititionString = "all_reps", verbose = True):
+def combineReps(parentDirectory, expectedRepetitions, repetitionStringBase = "rep", combinedRepetitionString = "all_reps", verbose = True):
 
-    firstRepString = f"{repititionStringBase}1"
+    firstRepString = f"{repetitionStringBase}1"
 
     # Iterate through the given directory
     for item in os.listdir(parentDirectory):
@@ -19,36 +19,36 @@ def combineReps(parentDirectory, expectedRepititions, repititionStringBase = "re
         # Recursively search directories
         if os.path.isdir(path): 
             if verbose: print(f"\nRecursing into directory: {item}")
-            combineReps(path, expectedRepititions, repititionStringBase, combinedRepititionString, verbose)
+            combineReps(path, expectedRepetitions, repetitionStringBase, combinedRepetitionString, verbose)
 
-        # Check for the repitition string, and then check for other repititions to combine.
+        # Check for the repetition string, and then check for other repetitions to combine.
         if firstRepString in item:
 
             if verbose: 
-                print(f"Found item with first repitition string: {item}")
-                print("Searching for other repititions...")
-            repititionFilePaths = [path]
+                print(f"Found item with first repetition string: {item}")
+                print("Searching for other repetitions...")
+            repetitionFilePaths = [path]
 
-            for repNum in range(2, expectedRepititions+1):            
-                nextRepititionFilePath = path.replace(firstRepString, f"{repititionStringBase}{repNum}")
-                if os.path.exists(nextRepititionFilePath):
-                    if verbose: print(f"\tFound repitition {repNum}.")
-                    repititionFilePaths.append(nextRepititionFilePath)
-                else: warnings.warn(f"Expected repitition {repNum} at {nextRepititionFilePath}, but it does not exist.")
+            for repNum in range(2, expectedRepetitions+1):            
+                nextRepetitionFilePath = path.replace(firstRepString, f"{repetitionStringBase}{repNum}")
+                if os.path.exists(nextRepetitionFilePath):
+                    if verbose: print(f"\tFound repetition {repNum}.")
+                    repetitionFilePaths.append(nextRepetitionFilePath)
+                else: warnings.warn(f"Expected repetition {repNum} at {nextRepetitionFilePath}, but it does not exist.")
 
-            if verbose: print(f"\tCombining repititions...")
+            if verbose: print(f"\tCombining repetitions...")
             # Make sure all paths are either gzipped or not gzipped (no mixing)
-            assert (all(filePath.endswith(".gz") for filePath in repititionFilePaths) or
-                    all(not filePath.endswith(".gz") for filePath in repititionFilePaths)), (
-                        f"Non-uniform compression across repititions: {repititionFilePaths}"
+            assert (all(filePath.endswith(".gz") for filePath in repetitionFilePaths) or
+                    all(not filePath.endswith(".gz") for filePath in repetitionFilePaths)), (
+                        f"Non-uniform compression across repetitions: {repetitionFilePaths}"
                     )
-            if repititionFilePaths[0].endswith(".gz"): openFunction = gzip.open
+            if repetitionFilePaths[0].endswith(".gz"): openFunction = gzip.open
             else: openFunction = open
-            combinedRepititionsFilePath = path.replace(firstRepString, combinedRepititionString)
-            with openFunction(combinedRepititionsFilePath, 'w') as combinedRepititionsFile:
-                for repititionFilePath in repititionFilePaths:
-                    with openFunction(repititionFilePath, 'r') as repititionFile:
-                        shutil.copyfileobj(repititionFile, combinedRepititionsFile)
+            combinedRepetitionsFilePath = path.replace(firstRepString, combinedRepetitionString)
+            with openFunction(combinedRepetitionsFilePath, 'w') as combinedRepetitionsFile:
+                for repetitionFilePath in repetitionFilePaths:
+                    with openFunction(repetitionFilePath, 'r') as repetitionFile:
+                        shutil.copyfileobj(repetitionFile, combinedRepetitionsFile)
 
 
 def main():
@@ -63,10 +63,13 @@ def main():
     #Create the Tkinter UI
     with TkinterDialog(workingDirectory=workingDirectory) as dialog:
         dialog.createFileSelector("Parent Directory:", 0, directory = True)
-        dialog.createTextField("Expected Repititions:", 1, 0, defaultText='2')
+        dialog.createTextField("Expected Repetitions:", 1, 0, defaultText='2')
+        dialog.createTextField("Repetition String Base:", 2, 0, defaultText = "rep")
+        dialog.createTextField("Combined Repetition String:", 3, 0, defaultText = "all_reps")
 
     selections = dialog.selections
-    combineReps(selections.getIndividualFilePaths()[0], int(selections.getTextEntries()[0]))
+    combineReps(selections.getIndividualFilePaths()[0], int(selections.getTextEntries()[0]),
+                selections.getTextEntries()[1], selections.getTextEntries()[2])
 
 
 if __name__ == "__main__": main()
