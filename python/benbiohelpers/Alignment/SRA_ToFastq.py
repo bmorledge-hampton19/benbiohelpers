b083547de9fb8048e8da3776b728dacd4f9566d7
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2023-06-26 23:42:56 -0700

    Compressed inputs to a single file
    Names, if given, are expected alongside accession ID's in a tab-separated format, instead of in a separate file. (This will make it easier to add/remove entries without accidentally causing "frameshifts").

--- python/benbiohelpers/Alignment/SRA_ToFastq.py
+++ python/benbiohelpers/Alignment/SRA_ToFastq.py
@@ -8,57 +8,55 @@ from benbiohelpers.FileSystemHandling.DirectoryHandling import checkDirs
 
 # Given a file path to a list of run accession IDs and an optional file path to their corresponding names,
 # uses sra-tools to retrieve the fastq sequences.
-def sRA_ToFastq(runAccessionIDsFilePath, namesFilePath = None):
+def sRA_ToFastq(runAccessionIDsFilePath, getNamesFromCol2 = False):
     
-    # Get the run accession IDs from the input file
-    with open(runAccessionIDsFilePath, 'r') as runAccessionIDsFile:
-        runAccessionIDs = [line.strip() for line in runAccessionIDsFile]
-
-    # Determine the names for the output files.
-    if namesFilePath is None: names = runAccessionIDs
-    else: 
-        with open(namesFilePath, 'r') as namesFile:
-            names = [line.strip() for line in namesFile]
-
     startTime = time.time()
     gzippedFastqFiles = list()
-    for runAccessionID, name in zip(runAccessionIDs, names):
+    with open(runAccessionIDsFilePath, 'r') as runAccessionIDsFile:
+        for line in runAccessionIDsFile:
 
-        print(f"\nRetrieving reads for {runAccessionID}")
-        
+            # Retrieve the accession ID and name from the current line.
+            if getNamesFromCol2:
+                runAccessionID, name = line.strip().split('\t')
+            else:
+                runAccessionID = line.strip().split('\t')[0]
+                name = runAccessionID
 
-        # Generate the paths for output.
-        alignmentFilesDir = os.path.join(os.path.dirname(runAccessionIDsFilePath), name, "alignment_files")
-        checkDirs(alignmentFilesDir)
-        fastqOutputFilePath = os.path.join(alignmentFilesDir, name + ".fastq")
+            print(f"\nRetrieving reads for {runAccessionID}")
+            
 
-        # Run prefetch.
-        prefetchStartTime = time.time()
-        print("\nRunning prefetch...")
-        subprocess.check_call(("prefetch", "-p", "-O", alignmentFilesDir, runAccessionID))
-        print(f"Time to fetch: {time.time() - prefetchStartTime} seconds")
+            # Generate the paths for output.
+            alignmentFilesDir = os.path.join(os.path.dirname(runAccessionIDsFilePath), name, "alignment_files")
+            checkDirs(alignmentFilesDir)
+            fastqOutputFilePath = os.path.join(alignmentFilesDir, name + ".fastq")
 
-        # Run fasterqdump.
-        fasterqDumpStartTime = time.time()
-        print("\nRunning fasterq-dump...")
-        subprocess.check_call(("fasterq-dump", "-p", "-o", fastqOutputFilePath, runAccessionID), cwd = alignmentFilesDir)
-        print(f"Time to retrieve fastq: {time.time() - fasterqDumpStartTime} seconds")
+            # Run prefetch.
+            prefetchStartTime = time.time()
+            print("\nRunning prefetch...")
+            subprocess.check_call(("prefetch", "-p", "-O", alignmentFilesDir, runAccessionID))
+            print(f"Time to fetch: {time.time() - prefetchStartTime} seconds")
 
-        # gzip the results.
-        print("\ngzipping results...")
-        gzipStartTime = time.time()
-        for item in os.listdir(alignmentFilesDir):
-            if item.endswith(".fastq"): 
-                print(f"gzipping {item}")
-                subprocess.check_call(("gzip", "-f", os.path.join(alignmentFilesDir, item)))
-                gzippedFastqFiles.append(os.path.join(alignmentFilesDir, item+".gz"))
-        print(f"Time to gzip files: {time.time() - gzipStartTime} seconds")
+            # Run fasterqdump.
+            fasterqDumpStartTime = time.time()
+            print("\nRunning fasterq-dump...")
+            subprocess.check_call(("fasterq-dump", "-p", "-o", fastqOutputFilePath, runAccessionID), cwd = alignmentFilesDir)
+            print(f"Time to retrieve fastq: {time.time() - fasterqDumpStartTime} seconds")
 
-        # Delete prefetched directory
-        print("\nDeleting prefetched directory...")
-        shutil.rmtree(os.path.join(alignmentFilesDir,runAccessionID))
+            # gzip the results.
+            print("\ngzipping results...")
+            gzipStartTime = time.time()
+            for item in os.listdir(alignmentFilesDir):
+                if item.endswith(".fastq"): 
+                    print(f"gzipping {item}")
+                    subprocess.check_call(("gzip", "-f", os.path.join(alignmentFilesDir, item)))
+                    gzippedFastqFiles.append(os.path.join(alignmentFilesDir, item+".gz"))
+            print(f"Time to gzip files: {time.time() - gzipStartTime} seconds")
 
-        print(f"**Total time so far: {time.time() - startTime} seconds**")
+            # Delete prefetched directory
+            print("\nDeleting prefetched directory...")
+            shutil.rmtree(os.path.join(alignmentFilesDir,runAccessionID))
+
+            print(f"**Total time so far: {time.time() - startTime} seconds**")
 
     return gzippedFastqFiles
 
@@ -66,20 +64,11 @@ def sRA_ToFastq(runAccessionIDsFilePath, namesFilePath = None):
 def main():
 
     # Create the UI.
-    with TkinterDialog() as dialog:
-        dialog.createFileSelector("SRA run accession IDs:", 0, ("text file",".txt"))
-        with dialog.createDynamicSelector(1, 0) as namesDynSel:
-            namesDynSel.initCheckboxController("Specify Custom Names:")
-            namesDynSel.initDisplay(True, "namesFilePath").createFileSelector(
-                "Names File:", 0, ("text file",".txt")
-            )
+    with TkinterDialog(title = "SRA to Fastq") as dialog:
+        dialog.createFileSelector("SRA run accession IDs:", 0, ("text file",".txt"), ("Tab separated values file", ".tsv"))
+        dialog.createCheckbox("Get names from second column in file (tab-separated).", 1, 0)
 
-    # Retrieve values from user input.
-    selections = dialog.selections
-    if namesDynSel.getControllerVar(): namesFilePath = selections.getIndividualFilePaths("namesFilePath")[0]
-    else: namesFilePath = None
-
-    sRA_ToFastq(selections.getIndividualFilePaths()[0], namesFilePath)
+    sRA_ToFastq(dialog.selections.getIndividualFilePaths()[0], dialog.selections.getToggleStates()[0])
     
 
 if __name__ == "__main__": main()
