f5e95c902d62a237534981e6b0c6d7dada96326e
Benjamin Morledge-Hampton <brotherbenben@yahoo.com>
2023-12-05 13:59:46 -0800

    FindAdapters is more robust to 0 enrichment
    - Added the searchLengthLimit parameter which determines the maximum number of bases in each adapter sequences that are used for exact matching, starting from the 5' end. This helps rescue cases which otherwise have 0% enrichment.
    - The NoEnrichedAdapterError exception is thrown when no enriched adapters are found.

--- python/benbiohelpers/Alignment/FindAdapters.py
+++ python/benbiohelpers/Alignment/FindAdapters.py
@@ -1,11 +1,19 @@
 # This script takes a set of potential adapter sequences and a fastq file and
 # determines which adapters are enriched in the reads.
 import os, gzip
+from typing import List, Dict, TextIO
 from benbiohelpers.TkWrappers.TkinterDialog import TkinterDialog
 from benbiohelpers.FileSystemHandling.DirectoryHandling import checkDirs, getIsolatedParentDir
 from benbiohelpers.FileSystemHandling.FastaFileIterator import FastaFileIterator
 from benbiohelpers.FileSystemHandling.GetFileSubset import getFastqSubset
-from typing import List, TextIO
+from benbiohelpers.InputParsing.CheckForNumber import checkForNumber
+
+
+class NoEnrichedAdapterError(Exception):
+    """
+    An error class for when no adapters are enriched beyond 0%
+    """
+
 
 def getFastqEntrySequence(fastqFile: TextIO):
     """
@@ -16,8 +24,8 @@ def getFastqEntrySequence(fastqFile: TextIO):
     fastqFile.readline(); fastqFile.readline()
     return sequence
 
-
-def findAdapters(fastqFilePaths: List[str], adapterFilePath, threshold = 0.01, defaultToMax = True, aggregateOutput = False):
+def findAdapters(fastqFilePaths: List[str], adapterFilePath, threshold = 0.01, defaultToMax = True, aggregateOutput = False,
+                 searchLengthLimit = None):
     """
     Given one or more fastq files (in a list) and a fasta file of adapter sequences,
     determines which adapters are enriched in each file (using a subset of that file)
@@ -26,6 +34,8 @@ def findAdapters(fastqFilePaths: List[str], adapterFilePath, threshold = 0.01, d
     defaultToMax parameter is true, the adapter with the highest enrichment is chosen.
     - If the aggregateOutput parameter is true, all enriched adapters are output to the same file, whose path is
     determined by the the first fastq file path given. (Useful when preparing to trim paired-end data.)
+    - searchLengthLimit determines the maximum number of bases in each adapter sequence that are used for exact matching,
+    starting from the 5' end.
     """
 
     # Get the fasta entries from the adapter file.
@@ -33,6 +43,12 @@ def findAdapters(fastqFilePaths: List[str], adapterFilePath, threshold = 0.01, d
     with open(adapterFilePath, 'r') as adapterFile:
         for fastaEntry in FastaFileIterator(adapterFile, False):
             adapterFastaEntries.append(fastaEntry)
+
+    adapterSearchSequences: Dict[FastaFileIterator.FastaEntry:str] = dict()
+    for adapterFastaEntry in adapterFastaEntries:
+        if searchLengthLimit is None: adapterSearchSequences[adapterFastaEntry] = adapterFastaEntry.sequence
+        else: adapterSearchSequences[adapterFastaEntry] = adapterFastaEntry.sequence[:searchLengthLimit]
+
 
     # Loop through the given fastq files, looking for adapter enrichment in each.
     enrichedAdapters = list()
@@ -55,7 +71,7 @@ def findAdapters(fastqFilePaths: List[str], adapterFilePath, threshold = 0.01, d
 
         # Do some initialization
         totalSequences = 0
-        adapterCounts = {fastaEntry: 0 for fastaEntry in adapterFastaEntries}
+        adapterCounts = {fastaEntry:0 for fastaEntry in adapterFastaEntries}
 
         # Subset the fastq file and open the resulting file path.
         with openFunction(getFastqSubset(fastqFilePath, outputDir=tempDir), "rt") as fastqSubsetFile:
@@ -64,7 +80,7 @@ def findAdapters(fastqFilePaths: List[str], adapterFilePath, threshold = 0.01, d
             sequence = getFastqEntrySequence(fastqSubsetFile)
             while sequence:
                 for fastaEntry in adapterCounts:
-                    if fastaEntry.sequence in sequence: adapterCounts[fastaEntry] += 1
+                    if adapterSearchSequences[fastaEntry] in sequence: adapterCounts[fastaEntry] += 1
                 totalSequences += 1
                 sequence = getFastqEntrySequence(fastqSubsetFile)
 
@@ -92,6 +108,7 @@ def findAdapters(fastqFilePaths: List[str], adapterFilePath, threshold = 0.01, d
         
             if not foundEnrichedAdapter:
                 maxAdapter: FastaFileIterator.FastaEntry = max(adapterCounts, key = adapterCounts.get)
+                if adapterCounts[maxAdapter] == 0: raise NoEnrichedAdapterError(f"No adapters are enriched for {os.path.basename(fastqFilePath)}.")
                 percentage = adapterCounts[maxAdapter]/totalSequences * 100
                 if defaultToMax:
                     print("WARNING: No adapters meet enrichment threshold. "
@@ -113,12 +130,20 @@ def main():
                                         ("Gzipped fastq Files", ".fastq.gz"), ("fastq Files", ".fastq"),
                                         additionalFileEndings=[".fastq"])
         dialog.createFileSelector("Adapter file:", 1, ("Fasta File", ".fa"))
+        with dialog.createDynamicSelector(2, 0) as searchLengthLimitDS:
+            searchLengthLimitDS.initCheckboxController("Limit search length")
+            searchLengthLimitDS.initDisplay(True, "searchLengthLimit").createTextField("Search length limit:", 0, 0, defaultText = "42")
 
     # Sanitize inputs
     fastqFilePaths = [filePath for filePath in dialog.selections.getFilePathGroups()[0] if 
                       getIsolatedParentDir(filePath) != ".tmp"]
+    if searchLengthLimitDS.getControllerVar():
+        searchLengthLimit = checkForNumber(dialog.selections.getTextEntries("searchLengthLimit")[0], True,
+                                           lambda x:x>0, "Expected a positive-integer search length limit")
+    else:
+        searchLengthLimit = None
 
-    findAdapters(fastqFilePaths, dialog.selections.getIndividualFilePaths()[0])
+    findAdapters(fastqFilePaths, dialog.selections.getIndividualFilePaths()[0], searchLengthLimit = searchLengthLimit)
 
 
 if __name__ == "__main__": main()
