9c1681c8e09d8dc5690f293bec76e8a9130dd52a
Benjamin Morledge-Hampton <brotherbenben@yahoo.com>
2024-06-07 11:24:11 -0700

    Lots o' packaging updates!
    - Released current benbiohelpers version for jammy, focal, and bionic.
    - Exact files for bionic are not present here, but the necessary changes are described in packaging_on_bionic.txt.

--- python/setup.py
+++ python/setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r") as fh:
 
 setup(
     name="benbiohelpers",
-    version="1.2.2",
+    version="1.3.1",
     description='Helper scripts for use in various bioinformatics projects',
     long_description_content_type="text/markdown",
     url='https://github.com/bmorledge-hampton19/benbiohelpers',
     author='Ben Morledge-Hampton',
     author_email='b.morledge-hampton@wsu.edu',
     license='MIT',
-    python_requires='>=3.7',
+    python_requires='>=3.8',
+    install_requires=['plotnine'],
     packages=find_packages(),
     package_data={"benbiohelpers": ["TkWrappers/test_tube.png"]}
 )
